98dbbe0c9c6a0aaf0397ab9ed2d8d1f0df92d8ee
Amrit Rijal <rijalamrit720@gmail.com>
2024-12-01 23:11:41 +0545

    added tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
@@ -16,15 +14,7 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
+        tabBarBackground: TabBarBackground
       }}>
       <Tabs.Screen
         name="index"
@@ -39,6 +29,24 @@ export default function TabLayout() {
           title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
+      />
+      <Tabs.Screen
+        name='create'
+        options={{ 
+          title: 'Create',
+         }}
+      />
+      <Tabs.Screen
+        name="video"
+        options={{ 
+          title: 'Video',
+         }}
+      />
+      <Tabs.Screen 
+        name="profile"
+        options={{ 
+          title: 'Profile'
+         }}
       />
     </Tabs>
   );
