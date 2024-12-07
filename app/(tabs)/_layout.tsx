0658c49591abbd3d48ffe084d83e7ef25700f849
Amrit Rijal <rijalamrit720@gmail.com>
2024-12-07 21:04:29 +0545

    tab icons update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,67 +1,109 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import icons from '../../constants/icons';
+import { Image, ImageSourcePropType, View } from 'react-native';
+
+type TabIconProps = {
+  icon: ImageSourcePropType;
+  color: string;
+  name: string;
+  focused: boolean;
+};
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-
+  const TabIcon: React.FC<TabIconProps> = ({ icon, color, name, focused }) => {
+    return (
+      <View>
+        <Image
+          source={icon}
+          resizeMode='contain'
+        />
+      </View>
+    );
+  }
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarBackground: TabBarBackground
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="home"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.home}
+              color={color}
+              name="Home"
+              focused={focused}
+            />
+          )
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.explore}
+              color={color}
+              name='Explore'
+              focused={focused}
+
+            />
+          )
         }}
       />
       <Tabs.Screen
         name='create'
         options={{
           title: 'Create',
-          tabBarIcon: () => {
-        <TabIcon
-          icon={icons.create}
-        />
-      }
-         }}
+          tabBarIcon: ({ focused, color }) => (
+            <TabIcon
+              icon={icons.create}
+              color={color}
+              name="Create"
+              focused={focused}
+            />
+          )
+        }}
       />
       <Tabs.Screen
         name="video"
         options={{
           title: 'Video',
-          tabBarIcon: () => {
-        <TabIcon
-          icon={icons.reel}
-        />
-      }
-         }}
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.reel}
+              color={color}
+              name='Video'
+              focused={focused}
+            />
+          )
+        }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
-          tabBarIcon: () => {
+          tabBarIcon: ({ color, focused }) => (
             <TabIcon
               icon={icons.profile}
+              color={color}
+              name="Profile"
+              focused={focused}
             />
-          }
+          )
         }}
       />
     </Tabs>
--- constants/icons.ts
+++ constants/icons.ts
@@ -2,10 +2,12 @@ import home from '../assets/images/home.png';
 import explore from '../assets/images/explore.png';
 import create from '../assets/images/create.png';
 import reel from '../assets/images/reel.png';
+import profile from '../assets/images/profile.png';
 
 export default {
   home,
   explore,
   create,
-  reel
+  reel,
+  profile
 }
